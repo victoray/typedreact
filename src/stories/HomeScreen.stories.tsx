fef0f3a9768464f20e192e2a09f3cc0342c5459e
Victor Ay <viktoray007@gmail.com>
2019-10-26 10:28:52 +0300

    move homescreen stories to stories folder

--- src/stories/HomeScreen.stories.tsx
+++ src/stories/HomeScreen.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {storiesOf} from "@storybook/react";
 
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "../components/screens/HomeScreen";
 import {BrowserRouter} from "react-router-dom";
 
 storiesOf("HomeScreen", module)
